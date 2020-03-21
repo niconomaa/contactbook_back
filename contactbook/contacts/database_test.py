4d232d3f660bfb4a9b544bff318f637f64c6651b
Lukas Heilmann <lukas@familieheilmann.de>
2020-03-21 16:03:57 +0100

    Rename friends to contacts; add comment

--- contactbook/contacts/database_test.py
+++ contactbook/contacts/database_test.py
@@ -1,21 +1,30 @@
 from neo4j import GraphDatabase
 
-driver = GraphDatabase.driver("bolt://0.0.0.0:7687", auth=("neo4j", "contactbook"))
+'''
+    This is just for testing Neo4j connections!
+    This is not integrated into the Django app.
+'''
+
+
+driver = GraphDatabase.driver("bolt://0.0.0.0:7687", auth=("neo4j", "contactbook"), encrypted=False)
+
 
 def add_contact(tx, name, contact_name):
-    tx.run("MERGE (a:Contact {name: $name}) "
+    tx.run("MERGE (a:Contact {name: $name})"
            "MERGE (a)-[:HAS_TOUCHED]->(b:Contact {name: $contact_name})",
            name=name, contact_name=contact_name)
 
-def print_friends(tx, name):
+
+def print_contacts(tx, name):
     for record in tx.run("MATCH (a:Contact)-[:HAS_TOUCHED]->(b) WHERE a.name = $name "
                          "RETURN b.name ORDER BY b.name", name=name):
         print(record["b.name"])
+
 
 with driver.session() as session:
     session.write_transaction(add_contact, "Arthur", "Guinevere")
     session.write_transaction(add_contact, "Arthur", "Lancelot")
     session.write_transaction(add_contact, "Arthur", "Merlin")
-    session.read_transaction(print_friends, "Arthur")
+    session.read_transaction(print_contacts, "Arthur")
 
 driver.close()
