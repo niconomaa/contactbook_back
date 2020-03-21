64f376b41f8b94d091cae777c34119d1eea0b2f2
Lukas Heilmann <lukas@familieheilmann.de>
2020-03-21 17:05:00 +0100

    Adapt contact schema for neomodels

--- contactbook/contacts/schema.py
+++ contactbook/contacts/schema.py
@@ -1,13 +1,22 @@
 import graphene
+from graphene_django.types import DjangoObjectType
+from contacts.models import Contact
+
+'''
+NOTE:   It is not possible to use 'DjangoObjectType' with Neomodels since they do not inherit from Django base models.
+        For this reason we have to use the basic 'ObjectType' for Neomodels here. 
+'''
+
+class ContactType(graphene.ObjectType):
+    first_name = graphene.String()
+
 
 class Query(graphene.ObjectType):
-    name = 'Query'
-    description = '...'
+    all_contacts = graphene.List(ContactType)
 
-    hello = graphene.String()
+    def resolve_all_contacts(self, info, **kwargs):
+        return Contact.nodes.all()
 
-    def resolve_hello(root, info):
-        return 'World'
 
 schema = graphene.Schema(
     query = Query
