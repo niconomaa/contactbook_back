2ec9cb4f3a4893bed39fd0324a0b2005e62f420f
Lukas Schmidt <lukmicsch@Lukass-MBP.fritz.box>
2020-03-21 17:44:19 +0100

    ready to conflict

--- contactbook/contacts/schema.py
+++ contactbook/contacts/schema.py
@@ -1,5 +1,8 @@
 import graphene
+from graphene_django.types import DjangoObjectType
+from django.contrib.auth.models import User
 
+"""
 class Query(graphene.ObjectType):
     name = 'Query'
     description = '...'
@@ -11,4 +14,45 @@ class Query(graphene.ObjectType):
 
 schema = graphene.Schema(
     query = Query
+) """
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+        email = graphene.String(required=True)
+
+    def mutate(self, info, username, password, email):
+        user = User(
+            username=username,
+            email=email
+        )
+        user.set_password(password)
+        user.save()
+
+        return CreateUser(user=user)
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
+
+class Query(graphene.ObjectType):
+    name = 'Query'
+    description = '...'
+
+    user = graphene.List(
+        UserType
+    )
+
+    def resolve_user(self, info, **kwargs):
+        return User.objects.all()
+
+schema = graphene.Schema(
+    query = Query,
+    mutation = Mutation
 )
