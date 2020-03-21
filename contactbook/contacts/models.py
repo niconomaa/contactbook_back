34b4324ec329296a24530243bbc047f1bb20e9b0
Lukas Heilmann <lukas@familieheilmann.de>
2020-03-21 19:01:51 +0100

    Create ContactRel; remove attributes from Person model

--- contactbook/contacts/models.py
+++ contactbook/contacts/models.py
@@ -1,18 +1,28 @@
 from django.db import models
+import datetime
 
 from neomodel import (
     StringProperty,
     StructuredNode,
+    StructuredRel,
     RelationshipTo,
     RelationshipFrom,
-    Relationship
+    Relationship,
+    DateProperty,
+    BooleanProperty
 )
 
 
-class Person(StructuredNode):
-    first_name = StringProperty()
-    last_name = StringProperty()
-    mobile_phone = StringProperty()
-    email = StringProperty()
+class ContactRel(StructuredRel):
+    date = DateProperty(
+        default=lambda: datetime.datetime.now()
+    )
+    location = StringProperty()
 
-    contacted_persons = RelationshipTo('Person', 'HAS_TOUCHED')
+
+class Person(StructuredNode):
+    mobile_phone = StringProperty()
+    verified = BooleanProperty()
+    infected = BooleanProperty()
+
+    contacted_persons = RelationshipTo('Person', 'HAS_TOUCHED', model=ContactRel)
